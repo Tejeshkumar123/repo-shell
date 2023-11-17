1e48538b13af6d5d55227c8d98bc5ef53f8693e8
Tejeshkumar123 <151117861+Tejeshkumar123@users.noreply.github.com>
2023-11-17 12:04:13 +0530

    Update 03.catalogue.sh

--- 03.catalogue.sh
+++ 03.catalogue.sh
@@ -20,7 +20,7 @@ cp /root/repo-shell/catalogue.service /etc/systemd/system/catalogue.service
 echo -e "$color DOWNLOADING AND INSTALLING THE MONGODB SCHEMA$nocolor"
 cp /root/repo-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org-shell -y &>>${logfile}
-mongo --host mongodb-dev.sindhu.cloud <${app_path}/schema/catalogue.js &>>${logfile}
+mongo --host mongodb-dev.tejesh.cloud <${app_path}/schema/catalogue.js &>>${logfile}
 echo -e "$color ENABLEING AND STARTING THE CATALOGUE SERVICE$nocolor"
 systemctl daemon-reload
 systemctl enable catalogue &>>${logfile}
